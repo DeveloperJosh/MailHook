f61b7081acb86a61e7cc5dbac48894e5187364fc
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-09-01 00:50:26 -0400

    yes

---
+++ utils/database.py
@@ -0,0 +1,13 @@
+import os
+
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+client = pymongo.MongoClient(
+    f"mongodb+srv://{os.getenv('DATABASE_NAME')}:{os.getenv('DATABASE_PASS')}@cluster0.fbf7q.mongodb.net/cluster0?retryWrites=true&w=majority")
+
+db = client['cluster0']
+
+collection = db["cluster0"]
+modmail_collection = db["mod-mail"]
