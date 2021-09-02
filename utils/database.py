9f3e4f9dfe7d1ffaff96aefaf6ef817d538eaa2e
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-09-01 20:00:16 -0400

    No femboys allowed (jk)

--- utils/database.py
+++ utils/database.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 client = pymongo.MongoClient(
-    f"mongodb+srv://{os.getenv('DATABASE_NAME')}:{os.getenv('DATABASE_PASS')}@cluster0.fbf7q.mongodb.net/cluster0?retryWrites=true&w=majority")
+    f"mongodb+srv://{os.getenv('DATABASE_NAME')}:{os.getenv('DATABASE_PASS')}@cluster0.3pkiv.mongodb.net/cluster0?retryWrites=true&w=majority")
 
 db = client['cluster0']
 
 collection = db["cluster0"]
 modmail_collection = db["mod-mail"]
+prefix_collection = db["prefix"]
