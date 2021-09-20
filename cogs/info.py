61f65f716e442232e9e570166065d684ee6f5b53
nirlep5252 <hellonirlep@gmail.com>
2021-09-20 19:30:52 +0530

    added sexy botinfo command

--- cogs/info.py
+++ cogs/info.py
@@ -1,9 +1,33 @@
 import time
 import discord
+import pygit2
+import itertools
+import datetime
+
 from discord.ext import commands
 from handler import slash_command, InteractionContext
 from utils.bot import ModMail
 from typing import Union
+
+
+def format_commit(commit: pygit2.Commit) -> str:
+    # CREDITS: https://github.com/Rapptz/RoboDanny
+    short, _, _ = commit.message.partition('\n')
+    short_sha2 = commit.hex[0:6]
+    commit_tz = datetime.timezone(datetime.timedelta(minutes=commit.commit_time_offset))
+    commit_time = datetime.datetime.fromtimestamp(
+        commit.commit_time).astimezone(commit_tz)
+
+    offset = f'<t:{int(commit_time.astimezone(datetime.timezone.utc).timestamp())}:R>'
+    return f'[`{short_sha2}`](https://github.com/DeveloperJosh/MailHook/commit/{commit.hex}) {short} ({offset})'
+
+
+def get_commits(count: int = 3):
+    # CREDITS: https://github.com/Rapptz/RoboDanny
+    repo = pygit2.Repository('.git')
+    commits = list(itertools.islice(
+        repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL), count))
+    return '\n'.join(format_commit(commit) for commit in commits)
 
 
 class Info(commands.Cog):
@@ -29,6 +53,37 @@ Other links:
             url=f"https://discord.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot%20applications.commands",
             color=discord.Color.blurple()
         ).set_footer(text="Thank you very much! 💖"))
+
+    @commands.command(name="bot-info", help="Get some info about me!")
+    @slash_command(name="bot-info", help="Get some info about me!")
+    async def botinfo(self, ctx: Union[commands.Context, InteractionContext]):
+        await ctx.reply(embed=discord.Embed(
+            title="Info about me!",
+            description="Modern modmail for modern Discord servers.",
+            color=discord.Color.blurple(),
+            timestamp=datetime.datetime.utcnow()
+        ).add_field(
+            name="Stats:",
+            value=f"""
+**Servers:** {len(self.bot.guilds)}
+**Users:** {len(self.bot.users)}
+**Commands:** {len(self.bot.commands)}
+            """,
+            inline=True
+        ).add_field(
+            name="Links:",
+            value=f"""
+- [Support Server](https://discord.gg/TeSHENet9M)
+- [Github](https://github.com/DeveloperJosh/MailHook)
+- [Invite](https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot%20applications.commands)
+            """,
+            inline=True
+        ).add_field(
+            name="Latest Commits:",
+            value=get_commits(),
+            inline=False
+        ).set_footer(text=self.bot.user.name, icon_url=self.bot.user.display_avatar.url
+        ).set_author(name=self.bot.user.name, icon_url=self.bot.user.display_avatar.url))
 
     @commands.command(name="credits", help="Credits to our contributors and helpers!")
     @slash_command(name="credits", help="Credits to our contributors and helpers!")
