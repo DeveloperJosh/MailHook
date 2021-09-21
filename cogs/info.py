268f75f06df8583f7d6cef9d279888f81b978963
DeveloperJosh <bam0909@outlook.com>
2021-09-20 23:20:31 -0400

    yes

--- cogs/info.py
+++ cogs/info.py
@@ -10,24 +10,24 @@ from utils.bot import ModMail
 from typing import Union
 
 
-def format_commit(commit: pygit2.Commit) -> str:
+#def format_commit(commit: pygit2.Commit) -> str:
     # CREDITS: https://github.com/Rapptz/RoboDanny
-    short, _, _ = commit.message.partition('\n')
-    short_sha2 = commit.hex[0:6]
-    commit_tz = datetime.timezone(datetime.timedelta(minutes=commit.commit_time_offset))
-    commit_time = datetime.datetime.fromtimestamp(
-        commit.commit_time).astimezone(commit_tz)
+#    short, _, _ = commit.message.partition('\n')
+#    short_sha2 = commit.hex[0:6]
+#    commit_tz = datetime.timezone(datetime.timedelta(minutes=commit.commit_time_offset))
+#    commit_time = datetime.datetime.fromtimestamp(
+#        commit.commit_time).astimezone(commit_tz)
 
-    offset = f'<t:{int(commit_time.astimezone(datetime.timezone.utc).timestamp())}:R>'
-    return f'[`{short_sha2}`](https://github.com/DeveloperJosh/MailHook/commit/{commit.hex}) {short} ({offset})'
+ #   offset = f'<t:{int(commit_time.astimezone(datetime.timezone.utc).timestamp())}:R>'
+  #  return f'[`{short_sha2}`](https://github.com/DeveloperJosh/MailHook/commit/{commit.hex}) {short} ({offset})'
 
 
-def get_commits(count: int = 3):
-    # CREDITS: https://github.com/Rapptz/RoboDanny
-    repo = pygit2.Repository('.git')
-    commits = list(itertools.islice(
-        repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL), count))
-    return '\n'.join(format_commit(commit) for commit in commits)
+#def get_commits(count: int = 3):
+#    # CREDITS: https://github.com/Rapptz/RoboDanny
+##    repo = pygit2.Repository('.git')
+ #   commits = list(itertools.islice(
+##        repo.walk(repo.head.target, pygit2.GIT_SORT_TOPOLOGICAL), count))
+#    return '\n'.join(format_commit(commit) for commit in commits)
 
 
 class Info(commands.Cog):
